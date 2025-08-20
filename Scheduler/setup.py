e4ece4699f14d7b3fd5687c4ff97e1611b6f159c
Neutrino <juliorivasantonio@gmail.com>
2025-08-20 13:15:36 +0200

    Update setup.py

--- Scheduler/setup.py
+++ Scheduler/setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Auto-Scheduler",
+    name="PyScheduler",
     version="0.1.0",
     author="Neutrino-102112915",
     description="A simple Python auto scheduler for tasks",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/Neutrino-122112915/Auto-Scheduler",
+    url="https://github.com/Neutrino-122112915/PyScheduler",
     packages=find_packages(),
     python_requires='>=3.6',
     install_requires=[

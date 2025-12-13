281fc651286318cbccb2a7a5806709fc0769721f
Neutrino <juliorivasantonio@gmail.com>
2025-12-13 13:53:29 +0100

    Update package name and URL in setup.py

--- Scheduler/setup.py
+++ Scheduler/setup.py
@@ -1,21 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="PyScheduler",
+    name="pyscheduler",
     version="0.1.0",
     author="Neutrino-102112915",
     description="A simple Python auto scheduler for tasks",
-    long_description=open("README.md").read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Neutrino-122112915/PyScheduler",
+    url="https://github.com/Neutrino-102112915/PyScheduler",
     packages=find_packages(),
-    python_requires='>=3.6',
-    install_requires=[
-        # Add dependencies if any
-    ],
+    python_requires=">=3.6",
+    install_requires=[],
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
 )
+
